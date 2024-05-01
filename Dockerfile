a73a6fae2a165de8cb40bcb8a5b6c7d996cc3522
Javad Jazandari <pash.u2048@gmail.com>
2024-05-01 13:37:32 +0330

    test

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,10 @@
 FROM ubuntu:22.04
 
 
+RUN apt-get -y update 
+RUN apt-get install -y curl sudo iptables
 COPY setup.sh /setup.sh
 
 RUN chmod +x /setup.sh
 CMD /setup.sh
-RUN echo $DST_SERVER_IP
+# RUN echo $DST_SERVER_IP
--- setup.sh
+++ setup.sh
@@ -5,22 +5,23 @@ set -e
 
 ## Replace DST_SERVER_IP and DST_SERVER_PORT with actual value
 # DST_SERVER_IP="2.2.2.2"
-apt-get -y update; apt-get -y install curl
-DST_SERVER_IP=$(curl "echoip.ir")
-DST_SERVER_PORT="22222"
+# apt-get -y update; apt-get -y install curl iptables
+DST_SERVER_IP="185.80.196.112"
+DST_SERVER_PORT="30008"
+# DST_SERVER_IP="185.80.196.112"
+# DST_SERVER_PORT="30008"
 
 ## Replace RELAY_SERVER_IP and RELAY_SERVER_PORT with actual value
-RELAY_SERVER_IP="185.80.196.112"
-RELAY_SERVER_PORT="30008"
+RELAY_SERVER_IP="37.32.21.176"
+RELAY_SERVER_PORT="22222"
 
-sudo iptables-save > "iptables-rules-backup-$(date '+%Y-%m-%d-%H:%M:%S').v4"
+# sudo iptables-save > "iptables-rules-backup-$(date '+%Y-%m-%d-%H:%M:%S').v4"
 
-echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward
+# echo 1 | tee /proc/sys/net/ipv4/ip_forward
 
-sudo iptables -t nat -A PREROUTING -p tcp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
-sudo iptables -t nat -A PREROUTING -p udp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
+sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
+sudo iptables -t nat -A PREROUTING -p udp -m udp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
 
-sudo iptables -t nat -A POSTROUTING -p tcp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
-sudo iptables -t nat -A POSTROUTING -p udp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
-
+sudo iptables -t nat -A POSTROUTING -p tcp -m tcp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
+sudo iptables -t nat -A POSTROUTING -p udp -m udp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
 ping 4.2.2.4
