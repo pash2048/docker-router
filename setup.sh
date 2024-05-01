d67501cda0f2dc1118db83205ca8a3ebcef02fc3
Javad Jazandari <pash.u2048@gmail.com>
2024-05-01 13:41:03 +0330

    test

--- setup.sh
+++ setup.sh
@@ -19,9 +19,9 @@ RELAY_SERVER_PORT="22222"
 
 # echo 1 | tee /proc/sys/net/ipv4/ip_forward
 
-sudo iptables -t nat -A PREROUTING -p tcp -m tcp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
-sudo iptables -t nat -A PREROUTING -p udp -m udp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
+sudo iptables -t nat -A PREROUTING -p tcp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
+sudo iptables -t nat -A PREROUTING -p udp --dport "${RELAY_SERVER_PORT}" -j DNAT --to-destination "${DST_SERVER_IP}:${DST_SERVER_PORT}"
 
-sudo iptables -t nat -A POSTROUTING -p tcp -m tcp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
-sudo iptables -t nat -A POSTROUTING -p udp -m udp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
+sudo iptables -t nat -A POSTROUTING -p tcp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
+sudo iptables -t nat -A POSTROUTING -p udp -d "${DST_SERVER_IP}" --dport "${DST_SERVER_PORT}" -j SNAT --to-source "${RELAY_SERVER_IP}"
 ping 4.2.2.4
